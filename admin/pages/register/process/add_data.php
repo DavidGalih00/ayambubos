735065f1366e4bc1277a9aecbf953f832d1d065c
DavidGalih00 <davigalih@gmail.com>
2022-01-13 15:54:33 +0700

    test

---
+++ admin/pages/register/process/add_data.php
@@ -0,0 +1,37 @@
+<?php
+    include('../../../../koneksi/connection.php');
+
+    $id_customer        = $_POST['id_customer'];
+    $nama_customer      = $_POST['nama_customer'];
+    $jk_customer        = $_POST['jk_customer'];
+    $alamat_customer      = $_POST['alamat_customer'];
+    $email_customer      = $_POST['email_customer'];
+    $telp_customer      = $_POST['telp_customer'];
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
+
+    $query = "INSERT INTO customer VALUES ('$id_customer','$nama_customer','$jk_customer','$alamat_customer','$email_customer','$telp_customer',0)";
+    $query2 = "INSERT INTO user VALUES ('$username','$password','Customer','$id_customer',0)";
+
+    // menjalankan query isi data
+    if (mysqli_query($con, $query) && mysqli_query($con, $query2))
+    {
+        header("Location:../../../index.php"); 
+    }
+    else
+    {
+        echo "<script type='text/javascript'>
+	    alert('Login Gagal!')
+	    </script>";
+        header("Location:../index.php");
+    }
+
+    mysqli_close($con);
+
+?> 
---
+++ partials/footer.php
@@ -0,0 +1,34 @@
+<div class="footer_overlay"></div>
+		<footer class="footer">
+			<div class="footer_background" style="background-image:url(images/footer2.png)"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="footer_content d-flex flex-lg-row flex-column align-items-center justify-content-lg-start justify-content-center">
+							<a class="navbar-brand" href="#">
+								<img src="images/logoayam.png" width="90" height="90" alt="">
+							</a>
+							<div class="footer_logo"><a href="index.php">Ayam Bubos</a></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+	<script src="../js/jquery-3.2.1.min.js"></script>
+	<script src="../styles/bootstrap4/popper.js"></script>
+	<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+	<script src="../plugins/greensock/TweenMax.min.js"></script>
+	<script src="../plugins/greensock/TimelineMax.min.js"></script>
+	<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+	<script src="../plugins/greensock/animation.gsap.min.js"></script>
+	<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+	<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+	<script src="../plugins/Isotope/isotope.pkgd.min.js"></script>
+	<script src="../plugins/easing/easing.js"></script>
+	<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+	<script src="../js/custom.js"></script>
+</body>
+
+</html>
